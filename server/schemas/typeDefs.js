8ca246c030522f5a98879c885d89d63608812dc6
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-06 15:08:17 -0600

    typeDefs build done? Sign up not working

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int        
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: [User]
+    }
+
+    input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+}
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        deleteBook(bookId: ID!): User
+        login(email: String!, password: String!): Auth
+
+    }
+`;
+
+module.exports = typeDefs;
