d456df69531eb4d57a830db0e1e87ab4ea195f04
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-06 10:24:18 -0600

    initial commit. Server file converted to Apollo Server

--- README.md
+++
@@ -1,2 +0,0 @@
-# book-search-engine
-(Work in Progress) - A tool designed to allow a user to search a database for the book they want.
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+
+// Requiring the Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+const db = require('./config/connection');
+
+// Requiring typeDefs and Resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Creating the ApolloServer using typeDefs and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  });
+});
