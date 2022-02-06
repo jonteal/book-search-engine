b900d1c4886225402b845c57c5744cd40b5fac09
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-06 15:59:01 -0600

    error:  “Identifier ‘user’ has already been declared”

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,13 +6,19 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-            return User.findOne({ _id: userId });
+            if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id }).select(
+                "-__v -password"
+            );
+            return userData;
+            }
+            throw new AuthenticationError("You need to be logged in!");
         },
-    },
+        },
 
     Mutation: {
         createUser: async (parent, { user, email, password }) => {
-            const user = await User.create({ user, email, password });
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
@@ -52,7 +58,7 @@ const resolvers = {
             if (context.user) {
                 return await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId } } },
+                    { $pull: { savedBooks: { bookId: bookId } } },
                     { new: true }
                 );
             }
