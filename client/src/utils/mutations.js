7e726bb9b043682213cd70da0917c470a5f35894
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-08 22:39:01 -0600

    REMOVE_BOOK mutation created with $bookId variable

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,3 +40,21 @@ export const SAVE_BOOK = gql`
         }
     }
 `
+
+export const REMOVE_BOOK = gql`
+    mutation deleteBook($bookId: ID!) {
+        deleteBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
