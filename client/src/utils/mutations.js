6d866e46524c8d3ba79e3f6c22f6c4ba2a008a17
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-08 22:36:46 -0600

    SAVE_BOOK mutation built out using $bookData variable

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,20 @@ export const ADD_USER = gql`
     }
 `;
 
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            savedBooks{
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
